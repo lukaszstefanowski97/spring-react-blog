13d0595eae4ba85fdd12436bf401c80b0b627202
lukaszstefanowski97 <lukasz.stefanowski97@gmail.com>
2019-06-05 15:45:46 +0200

    bug fixes

--- src/main/java/com/example/blog/rest/WebController.java
+++ src/main/java/com/example/blog/rest/WebController.java
@@ -2,7 +2,6 @@ package com.example.blog.rest;
 
 import com.example.blog.config.Messages;
 import com.example.blog.dto.Post;
-import com.example.blog.dto.ResponseObject;
 import com.example.blog.service.GetPostService;
 import com.example.blog.service.RemovePostService;
 import com.example.blog.service.SavePostService;
@@ -13,11 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import static com.example.blog.config.DataWorkflow.entries;
 import static com.example.blog.config.Messages.*;
-import static com.example.blog.config.Messages.POST_NOT_FOUND;
 
 @Controller
 @Slf4j
@@ -96,6 +96,8 @@ public class WebController {
             return "posts";
         }
         model.addAttribute("updateMessage", POST_NOT_FOUND);
+        model.addAttribute("post", new Post());
+        model.addAttribute("posts", getPostService.getAllPosts());
         return "posts";
     }
 
